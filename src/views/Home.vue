66cb5cecf1dcea387d519f113ccbb89b1011aa42
francisco-eduqo <francisco@eduqo.com.br>
2021-03-29 20:57:54 -0300

    Testing circle ci

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript TEST App" />
+    <HelloWorld msg="Welcome to My Vue.js + TypeScript Demo App" />
   </div>
 </template>
 
