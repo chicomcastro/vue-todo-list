f36cd1ab73e12c8a3f0c3ed61ded046ca28dce2b
francisco-eduqo <francisco@eduqo.com.br>
2021-04-03 00:54:34 -0300

    Testing changes to deploy

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to my TodoList app" />
+    <HelloWorld msg="Welcome to my TodoList vue app" />
   </div>
 </template>
 
