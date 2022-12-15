c436303bb1b156ed20da2b2e396943d04c84fb27
chicomcastro <franciscommcastro@gmail.com>
2022-12-15 00:34:46 +0000

    [ci skip] gh-pages

---
+++ precache-manifest.6095482ac1f410ba3ce94c5a15d11a23.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5861be4370e713258ec4",
+    "url": "/vue-todo-list/css/about.ea13aa55.css"
+  },
+  {
+    "revision": "4bae2730b36da54f2b17",
+    "url": "/vue-todo-list/css/app.00c2beb0.css"
+  },
+  {
+    "revision": "b604ed471d4aa07652c5",
+    "url": "/vue-todo-list/css/chunk-vendors.b7fdd020.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/vue-todo-list/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "f8bd706068a16fa8926da6b4fa1832cb",
+    "url": "/vue-todo-list/index.html"
+  },
+  {
+    "revision": "5861be4370e713258ec4",
+    "url": "/vue-todo-list/js/about.fc54ed87.js"
+  },
+  {
+    "revision": "4bae2730b36da54f2b17",
+    "url": "/vue-todo-list/js/app.5cee735a.js"
+  },
+  {
+    "revision": "b604ed471d4aa07652c5",
+    "url": "/vue-todo-list/js/chunk-vendors.50c67de1.js"
+  },
+  {
+    "revision": "3c378ecaa5aeca90acbbfd4d0f55daec",
+    "url": "/vue-todo-list/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue-todo-list/robots.txt"
+  }
+]);
